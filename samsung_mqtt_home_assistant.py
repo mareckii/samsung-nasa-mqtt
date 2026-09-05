b26d6c595c9d3c64f72f90e3f654a72b32e2c9cd
maro <mjsolarski@gmail.com>
2026-09-06 00:14:29 +0200

    Another test

--- samsung_mqtt_home_assistant.py
+++ samsung_mqtt_home_assistant.py
@@ -824,8 +824,9 @@ def mqtt_setup():
                         "device_class": 'power_factor',
                         "device": mqtt_discovery_device()}),
     retain=True)
+  prefix = (args.nasa_mqtt_prefix + ' ') if len(args.nasa_mqtt_prefix) > 0 else ''
   mqtt_client.publish('homeassistant/sensor/samsung_ehs_carnot_pct_cop/config', 
-    payload=json.dumps({"name": "% of Carnot CoP",
+    payload=json.dumps({"name": prefix + "% of Carnot CoP",
                         "state_topic": 'homeassistant/sensor/samsung_ehs_carnot_cop_pct/state',
                         "device_class": 'power_factor',
                         'unit_of_measurement': "%",
